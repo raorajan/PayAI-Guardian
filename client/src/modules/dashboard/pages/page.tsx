f663c835476d1e1a617d0d514a2261e826bf110e
Rajan Kumar <raorajan9576@gmail.com>
2026-04-13 20:53:57 +0530

    Clear the unused files

--- client/src/modules/dashboard/pages/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function dashboardPage() {
-  return <div className="p-6">dashboard Module Page</div>;
-}
--- client/src/modules/fraud-shield/pages/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function fraudShieldPage(): React.ReactElement {
-  return <div className="p-6">fraud-shield Module Page</div>;
-}
